b9714df58c809dc5c2d7929122e61f3af2ea66bb
Javier Ailbirt <jailbirt@gmail.com>
2016-03-30 18:06:18 -0300

    node_env

--- run.debug.sh
+++ run.debug.sh
@@ -17,8 +17,12 @@ echo -e "\e[92m"
 echo "using NODE_ENV=development"
 echo -e "\e[39m"
 
+if [ -z $NODE_ENV ];then
+   NODE_ENV=development
+fi
+
 cd $path;
 
-PORT=6080 DEBUG=eye:$debug NODE_ENV=development $nodemon $path/app.js
+PORT=6080 DEBUG=eye:$debug  $nodemon $path/app.js
 
 exit 0;
