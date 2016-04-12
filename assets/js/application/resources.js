0bf6e7eea617d2558fbc5f2e02c12137a03a85f6
Christian Gastrell <cgastrell@gmail.com>
2016-04-12 19:47:00 -0300

    implemented rowSelector, massChecker and massDelete on monitors
    also added tags for monitors with status=resource.state for easy finding

--- assets/js/application/resources.js
+++ assets/js/application/resources.js
@@ -15,7 +15,7 @@ $(function(){
     inputs.each(function(){
       var input = this;
       if(input.name=='disabled' && input.type=='checkbox'){
-        values['enable'] = !input.checked;
+        values.enable = !input.checked;
       } else if(input.name=='hosts_id') {
         if( !values[input.name] ) values[input.name]=[];
         values[input.name].push(input.value);
@@ -74,14 +74,14 @@ $(function(){
   function setCallback(type){
     return function(event){
       formSubmit(event,type);
-    }
+    };
   }
 
   $('.modal#processResourceModal button[type=submit]').on('click', setCallback('process'));
   $('.modal#scriptResourceModal button[type=submit]').on('click', setCallback('script'));
   $('.modal#scraperResourceModal button[type=submit]').on('click', setCallback('scraper'));
 
-  function fillForm($form, data){
+  function fillForm($uselsess_form, data){
     var resource = data.resource;
     var monitor = data.monitors[0];
     var type = monitor.type;
@@ -140,14 +140,14 @@ $(function(){
           $('.modal#scriptResourceModal').on('hidden.bs.modal', function(e) {
             $script.off('change');
             $disable.off('change');
-          })
+          });
         }
 
         $script.val(monitor.config.script_id);
         $form.find('[data-hook=script_arguments]')
           .val(monitor.config.script_arguments);
       break;
-    };
+    }
   }
 
   function setupEditResourceForm($form, options){
@@ -286,7 +286,7 @@ $(function(){
 
     var $form = $('#dstatResourceModal form');
     var idResource = $form.find('[data-hook=resource_id]').val();
-    var values = $form.serializeArray()
+    var values = $form.serializeArray();
 
     values.push({
       'name':'monitor_type',
@@ -367,4 +367,202 @@ $(function(){
       .find('button.search')
       .trigger('click');
   });
+
+  /* ROW SELECTOR + MASS CHECKER + MASS DELETE */
+  (function(){
+    // searchbox hook
+    $searchbox.on('search:start', function() {
+      $('.massChecker').trigger('uncheck');
+    });
+    $searchbox.on('search:empty', function() {
+      $('.massChecker').trigger('uncheck');
+    });
+
+    // SETUP
+    var firstConfirmHeader = '<h1>Massive monitor delete</h1>Heads up!<br /><br />';
+    var firstConfirmFooter = '<br />will be deleted.<h2>Are you sure?</h2>';
+    var secondConfirmHeader = '<h1>Wait, really sure?</h1>' +
+      'Please review the list, just in case:<br /><br />';
+    var secondConfirmFooter = '<br />WILL BE DELETED<h2>Confirm wisely</h2>';
+    var successTitle = 'Monitors deleted';
+    var successFooter = '<br/>...you will be missed :(';
+    var failTitle = 'Monitors deleted (some)';
+    var failFooter = '<br/>...I tried to delete these monitors' +
+      ' yet some of them came back with errors.' +
+      '<br /><br />Please refresh now';
+    var dataId = "itemId"; // the data-something where we get the id of the item
+    var dataDescriptor = "itemName"; // the data-something where we get the name of the item
+    var listTemplate = "{descriptor} ({id})<br />";
+    var itemSelector = 'div.itemRow.selectedItem:visible';
+    var apiUrlEndpoint = '/resource/';
+    var apiRequestType = 'DELETE';
+
+    // MASS DELETE - OJO
+    $('.massDelete').on('click', function(evt){
+      evt.preventDefault();
+      var taskRows = "";
+      var taskIds = [];
+      //collect selected rows.data (dataId & dataDescriptor)
+      $('.itemRow.selectedItem:visible').each(function(i,e){
+        var itemId = $(e).data(dataId);
+        var itemName = $(e).data(dataDescriptor);
+        if(itemId) {
+          taskIds.push(itemId);
+          var listItem = listTemplate
+            .replace("{id}", itemId)
+            .replace("{descriptor}", itemName);
+          //concatenate notification rows
+          taskRows = taskRows + listItem;
+        }
+      });
+      if(taskRows) {
+        bootbox.confirm(firstConfirmHeader + taskRows + firstConfirmFooter, function(result1){
+          if(!result1) {
+            return;
+          }
+          bootbox.confirm(secondConfirmHeader + taskRows + secondConfirmFooter, function(result2){
+            if(!result2) {
+              return;
+            }
+            $.blockUI();
+            var deleteRequests = [];
+            var removeOnSuccess = function(response, status, xhr) {
+              console.log('request success');
+              console.log(arguments);
+              // horrible kludge:
+              // response is "Task TASK_ID deleted"
+              // split by " " and take 2nd result as taskId
+              // API should respond with only id or {taskId: ID}
+              if(status === "success") {
+                $('div.itemRow[data-item-id='+response.split(" ")[1]+']').remove();
+              }
+            };
+            for(var ii = 0; ii < taskIds.length; ii++) {
+              deleteRequests.push(
+                $.ajax({
+                  url: apiUrlEndpoint + taskIds[ii],
+                  type: apiRequestType,
+                  // on success remove div[data-item-id=itemId]
+                  success: removeOnSuccess
+                })
+              );
+            }
+
+            $.when.apply($, deleteRequests).then(
+              function(){
+                console.log('then success');
+                console.log(arguments);
+                alert(taskRows + successFooter, successTitle);
+              },function(){
+                console.log('then fail');
+                console.log(arguments);
+                alert(taskRows + failFooter, failTitle);
+              },
+              // then progress nunca se llama ... ?
+              function() {
+                console.log('then progress');
+                console.log(arguments);
+              }
+            // when progress nunca se llama tampoco ... ?
+            ).progress(function(){
+                console.log('when progress');
+                console.log(arguments);
+              }
+            ).always(function(){
+                console.log('always');
+                $.unblockUI();
+              }
+            ).done(function(){
+              // done deberia volver con array de results
+              // no se si no funciona o es porque el req.DELETE
+              // no devuelve nada, habria que probar de cambiar la API
+              console.log('when done');
+              console.log(arguments);
+              console.log('ok, they are gone');
+            });
+          });
+        });
+      }
+      $(this).blur();
+      return false;
+    });
+
+    // MASS CHECKER
+    $('.massChecker').on('click', function(evt){
+      var $this = $(this);
+      evt.stopPropagation();
+      var $spanIcon = $this.children('span').first();
+      if($this.data('checked')) {
+        //uncheck all by firing event
+        $this.trigger('uncheck');
+      }else{
+        // do an "uncheck all", there maybe some left from a prior search
+        // this should be hooked on the search event
+        $('.rowSelector').trigger('uncheck');
+        $('.rowSelector:visible').trigger('check');
+        $this.data('checked',true);
+        $spanIcon.addClass('glyphicon-check');
+        $spanIcon.removeClass('glyphicon-unchecked');
+      }
+      $(this).blur();
+    });
+    $('.massChecker').on('uncheck', function(evt){
+      var $this = $(this);
+      var $spanIcon = $this.children('span').first();
+      $this.data('checked',false);
+      $spanIcon.removeClass('glyphicon-check');
+      $spanIcon.addClass('glyphicon-unchecked');
+      $('.rowSelector').trigger('uncheck');
+    });
+
+    // MASS CHECKER: when an item changes state it triggers
+    // a itemchanged event on massChecker. On itemchanged MASS CHECKER
+    // checks for an unchecked item. If any, MASS CHECKER unchecks itself
+    $('.massChecker').on('itemchanged', function(evt){
+      console.log('checking items state');
+      var $this = $(this);
+      var $spanIcon = $this.children('span').first();
+      $('.rowSelector:visible').each(function(i,e){
+        if(!$(e).data('checked')) {
+          $this.data('checked', false);
+          $spanIcon.removeClass('glyphicon-check');
+          $spanIcon.addClass('glyphicon-unchecked');
+          return;
+        }
+      });
+    });
+
+    // ROW SELECTOR
+    $('.rowSelector').on('check', function(evt){
+      var $this = $(this);
+      var $spanIcon = $this.children('span').first();
+      $this.data('checked',true);
+      $spanIcon.addClass('glyphicon-check');
+      $spanIcon.removeClass('glyphicon-unchecked');
+      $this.closest('.itemRow').addClass('selectedItem');
+    });
+    $('.rowSelector').on('uncheck', function(evt){
+      var $this = $(this);
+      var $spanIcon = $this.children('span').first();
+      $this.data('checked',false);
+      $spanIcon.removeClass('glyphicon-check');
+      $spanIcon.addClass('glyphicon-unchecked');
+      $this.closest('.itemRow').removeClass('selectedItem');
+    });
+
+    // ROW SELECTOR on click only determine if checked or not and fire event
+    $('.rowSelector').on('click', function(evt){
+      var $this = $(this);
+      evt.stopPropagation();
+      evt.preventDefault();
+      if($this.data('checked')) {
+        $this.trigger('uncheck');
+        // Notify MASS CHECKER
+        $('.massChecker').trigger('itemchanged');
+      }else{
+        $this.trigger('check');
+      }
+      $this.blur();
+    });
+  })();
 });
