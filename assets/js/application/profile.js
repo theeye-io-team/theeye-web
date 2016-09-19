fe9be6a35e3f13a774347b9a9e22fb58110f909c
Christian Gastrell <cgastrell@gmail.com>
2016-09-19 00:35:39 -0300

    added clipboard functionality for tokens, added hidden class and toggle when blurred items are blurred

--- assets/js/application/profile.js
+++ assets/js/application/profile.js
@@ -1,11 +1,14 @@
 $('document').ready(function(){
 
   new Clipboard('.clipboard-btn');
+  new Clipboard('.clipboard-token-btn');
+  new Clipboard('.clipboard-agent-token-btn');
 
   $('[data-hook=blurry]').on('click',function(event){
     $( this ).toggleClass('blurry-text');
   });
 
   $('[data-hook=blurry]').addClass('cursor-pointer blurry-text');
+  $('[data-hook=blurry]').removeClass('hidden');
 
 });
