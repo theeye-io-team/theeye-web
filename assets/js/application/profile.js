116a58a8dd072d10c232306bbb5f590f25cf8aeb
Christian Gastrell <cgastrell@gmail.com>
2016-09-19 19:58:17 -0300

    extended clipboard functionality to other fields

--- assets/js/application/profile.js
+++ assets/js/application/profile.js
@@ -1,8 +1,12 @@
+/* global Clipboard */
 $('document').ready(function(){
 
   new Clipboard('.clipboard-btn');
   new Clipboard('.clipboard-token-btn');
   new Clipboard('.clipboard-agent-token-btn');
+  new Clipboard('.clipboard-client-secret-btn');
+  new Clipboard('.clipboard-client-id-btn');
+  new Clipboard('.clipboard-customer-name-btn');
 
   $('[data-hook=blurry]').on('click',function(event){
     $( this ).toggleClass('blurry-text');
