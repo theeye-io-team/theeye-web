d662b79518d4bfaeb4e76ba4b85a7e98954279fe
Facugon <facugon@interactar.com>
2016-10-03 18:00:10 -0300

    some improvements. webhook crud page

---
+++ assets/js/app/model/webhook.js
@@ -0,0 +1,28 @@
+window.App || ( window.App = {} );
+window.App.Models || ( window.App.Models = {} );
+window.App.Models.Webhook = Backbone.Model.extend({
+  urlRoot:'/api/webhook',
+  parse:function(response){
+    response.hosts = [ response.host_id ];
+    return response;
+  },
+  initialize:function(){
+    Object.defineProperty(this,'triggerUrl',{
+      get: function() {
+        var cookie = Cookies.getJSON('theeye');
+        var url = cookie.supervisor_url;
+        var customer = cookie.customer;
+
+        return url + '/' + customer + '/webhook/' +
+          this.id + '/trigger?access_token=***' ;
+      }
+    });
+  }
+});
+
+
+window.App.Collections || ( window.App.Collections = {} );
+window.App.Collections.Webhooks = Backbone.Collection.extend({
+  model: window.App.Models.Webhook,
+  url:'/api/webhook'
+});
--- assets/js/app/view/base-view.js
+++ assets/js/app/view/base-view.js
@@ -1,11 +1,52 @@
+function CollectionRenderer (options) {
+  var View = options.View;
+  var container = options.container;
+  var collection = options.collection;
+
+  var views = [];
+
+  function renderItem (item) {
+    var view = new View({ model: item });
+    view.render();
+
+    views.push( view );
+
+    container.appendChild( view.el );
+  }
+
+  collection.forEach(renderItem);
+
+  function reRenderCollection(){
+    views.forEach(function(view){ view.remove(); });
+    if( collection.length > 0 ){
+      collection.forEach(renderItem);
+    }
+  }
+
+  //
+  // keep the collection-view updated
+  //
+  collection.on('add',renderItem,this);
+  collection.on('remove',function(){ console.log('item removed'); },this);
+  collection.on('change',function(){ console.log('item updated'); },this);
+  collection.on('sync',reRenderCollection);
+  collection.on('reset',reRenderCollection);
+}
+
+
+
 
 var BaseView = Backbone.View.extend({
+  initialize:function(){
+    if( this.autoRender ) this.render();
+  },
   renderTemplate: function(){
-    var html = this.template();
+    var html = this.template(this.model||{});
     this.$el.html( html );
 
-    if( this.targetEl )
-      this.targetEl.appendChild( this.$el[0] );
+    if( this.container ) {
+      this.container.appendChild( this.el );
+    }
 
     return this;
   },
@@ -17,5 +58,12 @@ var BaseView = Backbone.View.extend({
   },
   render: function(){
     this.renderTemplate();
+  },
+  renderCollection: function(collection, View, container) {
+    new CollectionRenderer({
+      collection: collection,
+      View: View,
+      container: container
+    });
   }
 });
