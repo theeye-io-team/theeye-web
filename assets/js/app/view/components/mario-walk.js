5b32b1eb2e9017c5ae2eec1f2260b770d5dc159a
Facugon <facugon@interactar.com>
2016-12-01 00:14:34 -0300

    mario walk

--- assets/js/app/view/components/mario-walk.js
+++ assets/js/app/view/components/mario-walk.js
@@ -11,9 +11,15 @@ var MarioWalk = function (options) {
 
   options||(options={});
 
-  var $template = $('<div style="position:fixed;bottom:0px;left:-100px;z-index:1000;"><img src=/mario/running.gif></div>');
+  var $mario_small = $('<div style="position:fixed;bottom:0px;left:-100px;z-index:1000;"><img src=/mario/mario_small.gif></div>');
+  var $mario_big = $('<div style="position:fixed;bottom:0px;left:-100px;z-index:1000;"><img width="100px" src=/mario/mario_big.gif></div>');
+  var $yoshi = $('<div style="position:fixed;bottom:0px;left:-100px;z-index:1000;"><img width="100px" src=/mario/yoshi_color.gif></div>');
+  var $mario_yoshi = $('<div style="position:fixed;bottom:0px;left:-150px;z-index:1000;"><img width="150px" src=/mario/mario_yoshi.gif></div>');
 
-  $template.appendTo(document.body);
+  $mario_small.appendTo(document.body);
+  $mario_big.appendTo(document.body);
+  $yoshi.appendTo(document.body);
+  $mario_yoshi.appendTo(document.body);
 
   if (options.full===true) {
     var $audio = $('<audio src="/mario/theme.mp3" preload="auto"></audio>');
@@ -22,10 +28,22 @@ var MarioWalk = function (options) {
   }
   $audio[0].play();
 
-  $template.animate({
-    left: $(window).width() 
-  },3600,'swing',function complete(){
-    /* on complete */ 
+  function animate ($el,next) {
+    $el.animate({
+      left: $(window).width() 
+    },3600,'swing',function complete(){
+    });
+    
+    setTimeout(next,500);
+  }
+
+  animate($mario_small,function(){
+    animate($mario_big,function(){
+      animate($yoshi,function(){
+        animate($mario_yoshi,function(){
+        });
+      });
+    });
   });
 
   return this;
