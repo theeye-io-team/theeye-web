7c862f3bc199f47ce647960ab87d651a283ad445
Facugon <facugon@interactar.com>
2017-01-25 17:56:42 -0300

    add category to help icons and assign to app.help

--- assets/js/app/view/components/help.js
+++ assets/js/app/view/components/help.js
@@ -1,12 +1,29 @@
+'use strict';
+
+window.App||(window.App={});
+window.App.Help = { icons: [] };
 
 var HelpIcon = BaseView.extend({
   tagName: 'span',
   className: 'glyphicon glyphicon-question-sign',
   autoRender: true,
+  initialize: function(options){
+    BaseView.prototype.initialize.apply(this,arguments);
+
+    this.category = (options.category||'help');
+
+    window.App.Help.icons.push( this );
+  },
   events: {
     'click': function(e) {
       if (this.link) window.open(this.link, '_blank');
     }
+  },
+  show: function(){
+    this.$el.tooltip('show');
+  },
+  hide: function(){
+    this.$el.tooltip('hide');
   },
   render: function() {
     var $el = this.$el;
