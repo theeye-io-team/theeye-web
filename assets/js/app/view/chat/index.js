7e1f4f6ba9a7347ecee23f33b7665a58b308fc68
Facundo Gonzalez <facugon@interactar.com>
2017-07-11 17:20:51 -0300

    remove unsupported uglyfy syntax

--- assets/js/app/view/chat/index.js
+++ assets/js/app/view/chat/index.js
@@ -8,13 +8,13 @@ const ChatBox = (function(){
 
   const ChatBoxBaloon = BaseView.extend({
     autoRender: true,
-    template: `
-    <div class="chat-box chat-box-baloon">
-      <a href="https://community.theeye.io" target="_blank">
-        <i class="fa fa-comment"></i>&nbsp;
-        <span>Chat with us.</span>
-      </a>
-    </div>`,
+    template: [
+    '<div class="chat-box chat-box-baloon">',
+      '<a href="https://community.theeye.io" target="_blank">',
+        '<i class="fa fa-comment"></i>&nbsp;',
+        '<span>Chat with us.</span>',
+      '</a></div>'
+    ].join(''),
     render: function () {
       this.renderTemplate()
 
