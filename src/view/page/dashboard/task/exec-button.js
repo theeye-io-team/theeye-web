ccdc582586abf8a7e6ad1c36f76082a893dad927
Facugon <facugon@interactar.com>
2017-10-17 12:21:12 -0300

    bug fix. inprogress is not a function

--- src/view/page/dashboard/task/exec-button.js
+++ src/view/page/dashboard/task/exec-button.js
@@ -28,7 +28,7 @@ module.exports = View.extend({
 
     if (!this.model.canExecute) return
 
-    if (this.model.lastjob.inProgress()) {
+    if (this.model.lastjob.inProgress) {
       const message = `Cancel task <b>${this.model.name}</b> execution?
         <a target="_blank" href="https://github.com/theeye-io/theeye-docs/blob/master/tasks/cancellation">Why this happens?</a>`
 
