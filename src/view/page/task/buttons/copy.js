835dc96baf12ca9645fa5b83716b2c10ca7dad58
Tomás Marcote <tomas.marcote@gmail.com>
2018-07-26 18:18:29 +0000

    Merged in copy-task-fix (pull request #329)
    fixed copy task

--- src/view/page/task/buttons/copy.js
+++ src/view/page/task/buttons/copy.js
@@ -1,81 +1,13 @@
 'use strict'
 
-import App from 'ampersand-app'
-import assign from 'lodash/assign'
 import PanelButton from 'components/list/item/panel-button'
 import Modalizer from 'components/modalizer'
-import FormView from 'ampersand-form-view'
-import InputView from 'components/input-view'
-import SelectView from 'components/select2-view'
-import TaskActions from 'actions/task'
-
-const CopyForm = FormView.extend({
-  initialize () {
-    this.fields = [
-      new SelectView({
-        label: 'Target Bots *',
-        name: 'hosts',
-        multiple: true,
-        tags: true,
-        options: App.state.hosts,
-        //value: null,
-        styles: 'form-group',
-        required: false,
-        unselectedText: 'select source Bot',
-        idAttribute: 'id',
-        textAttribute: 'hostname',
-        requiredMessage: 'Selection required',
-        invalidClass: 'text-danger',
-        validityClassSelector: '.control-label'
-      }),
-      new InputView({
-        label: 'New Name',
-        name: 'name',
-        required: false,
-        invalidClass: 'text-danger',
-        validityClassSelector: '.control-label',
-        value: `Copy of ${this.model.name}`,
-      })
-    ]
-
-    FormView.prototype.initialize.apply(this,arguments)
-  },
-  submit (next) {
-    this.beforeSubmit()
-    if (!this.valid) return
-
-    const data = this.prepareData(
-      assign({},this.model.serialize(),{
-        name: this.data.name,
-        task_arguments: this.model.task_arguments.serialize()
-      })
-    )
-    TaskActions.createMany(this.data.hosts, data)
-    if (next) next()
-    this.trigger('submitted')
-  },
-  prepareData (data) {
-    delete data.id
-    delete data._id
-    delete data.host
-    delete data.host_id
-    delete data.creation_date
-    delete data.last_update
-    delete data._type
-    return data
-  },
-  focus () {
-    this.query('input').focus()
-  },
-  render () {
-    FormView.prototype.render.apply(this, arguments)
-    this.query('form').classList.add('form-horizontal')
-  }
-})
+import bootbox from 'bootbox'
+import { Factory as TaskFactory } from 'models/task'
 
 module.exports = PanelButton.extend({
   initialize (options) {
-    PanelButton.prototype.initialize.apply(this,arguments)
+    this.title = 'Copy Task'
     this.tip = 'Copy Task'
     this.iconClass = 'fa fa-copy'
     this.className = 'btn btn-primary'
@@ -84,26 +16,37 @@ module.exports = PanelButton.extend({
     click (event) {
       event.stopPropagation()
 
-      const form = new CopyForm({ model: this.model })
-      const modal = new Modalizer({
-        confirmButton: 'Copy',
-        buttons: true,
-        title: 'Copy Task',
-        bodyView: form
-      })
+      return import(/* webpackChunkName: "task-form" */ '../form').then(FormView => {
 
-      this.listenTo(modal,'shown',() => { form.focus() })
-      this.listenTo(modal,'hidden',() => {
-        form.remove()
-        modal.remove()
-      })
-      this.listenTo(modal,'confirm',() => {
-        form.submit( () => {
+        const task = new TaskFactory({type: this.model.type})
+
+        const form = new FormView({
+          model: task
+        })
+
+        const modal = new Modalizer({
+          buttons: false,
+          title: this.title,
+          bodyView: form
+        })
+
+        this.listenTo(modal,'shown',() => {
+          form.focus()
+          form.setWithTask(this.model)
+          form._fieldViews.name.input.value = ''
+        })
+
+        this.listenTo(modal,'hidden',() => {
+          form.remove()
+          modal.remove()
+        })
+
+        this.listenTo(form,'submitted',() => {
           modal.hide()
         })
-      })
 
-      modal.show()
+        modal.show()
+      })
     }
   }
 })
