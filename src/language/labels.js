2e9e4b986a7f39782e7c90d035d7b9c56352759b
facugon <facugon@theeye.io>
2021-08-30 17:00:01 -0300

    change label

--- src/language/labels.js
+++ src/language/labels.js
@@ -9,7 +9,7 @@ export default {
           target_dynamic: 'Dynamically determined in the previous task',
           target_fixed: 'Specific Approvers (Choose below)'
         },
-        allows_behaviour_change: 'Allows to changes the behaviour of running jobs'
+        allows_behaviour_change: 'Allows to change the behaviour of running jobs'
       }
     }
   }
