7d15714624d263b5fa6135b5df3f26d83dd160fb
facugon <facugon@theeye.io>
2021-11-16 18:17:48 -0300

    google login enabled for alpha

--- src/config/alpha.js
+++ src/config/alpha.js
@@ -6,5 +6,32 @@ export default Object.assign({}, defaultConfigs, {
   socket_url: 'https://alpha.theeye.io',
   api_url: 'https://alpha.theeye.io/api',
   api_v3_url: 'https://alpha.theeye.io/api',
-  supervisor_api_url: 'https://supervisor-alpha.theeye.io:60080'
+  supervisor_api_url: 'https://supervisor-alpha.theeye.io:60080',
+  components: {
+    dynamic_form: {
+      remote: {
+        query_limit: 10
+      }
+    },
+    login: {
+      registration: {
+        enabled: true
+      },
+      password_reset: {
+        enabled: true 
+      },
+      domain: {
+        enabled: false
+      },
+      enterprise: {
+        enabled: false
+      },
+      google: {
+        enabled: true
+      }
+    },
+    grecaptcha: {
+      sitekey: '6LfaIrsUAAAAAKmZPeN5ZqmFDvJZLEdtK0dev2eV'
+    }
+  }
 })
