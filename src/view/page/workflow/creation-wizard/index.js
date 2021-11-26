436af7feceb416a719e5f45ebd10e2c9c2c54a24
Facundo <facugon@theeye.io>
2021-11-25 23:42:53 -0300

    feat: Workflow copy, export and import (#10)
    * fix: labels
    
    * Exporting workflows now possible
    
    * The copy workflow modal is displayed properly
    
    * Copy workflow view
    
    * Type settings
    
    * Copying workflows is now possible
    
    * remove ismongo check
    
    * improve code. unneeded views/props/deps removed
    
    * task forms submit method unified
    
    * add graphTasks and graphEvents derived
    
    * call task.create action to createMany tasks
    
    * add alias workflow = this.model
    
    * IN PROGRESS MUST BE TESTED
    
    * method added again. required and must be empty
    
    * remove unused code. add submit improves
    
    * validate
    
    * TMP
    
    * add initialization
    
    * workflow form . changes in progress
    
    * avoid adding temporal tasks to the state
    
    * add flag to models persisted in the api
    
    * add global persisted flag
    
    * copy / clone . create workflow
    
    * Added two buttons to connect tasks
    
    * add synchonized property
    
    * connect tasks
    
    * remove clicked node only
    
    * on remove workflow, remove tasks
    
    * added workflow version & migration on copy/update
    
    * improve version control using api headers
    
    * show workflow version
    
    Co-authored-by: Ezequiel Korenblum <ezequiel@losko.com.ar>

--- src/view/page/workflow/creation-wizard/index.js
+++ src/view/page/workflow/creation-wizard/index.js
@@ -1,9 +1,8 @@
 import App from 'ampersand-app'
-import View from 'ampersand-view'
 import Modalizer from 'components/modalizer'
 import HelpTexts from 'language/help'
 import HelpIconView from 'components/help-icon'
-import WorkflowFormView from '../form'
+import FormView from '../form'
 import config from 'config'
 
 import { Workflow } from 'models/workflow'
@@ -13,54 +12,31 @@ import './styles.less'
 const docsLink = 'core-concepts/tasks/tasks_workflows/'
 
 export default function () {
-  const wizard = new WorkflowCreationWizard()
-  wizard.render()
+
+  const workflow = new Workflow({ version: 2 })
+  const form = new FormView({ model: workflow, create: true })
 
   const modal = new Modalizer({
     buttons: false,
     title: 'Create Workflow',
-    bodyView: wizard
+    bodyView: form 
   })
 
   modal.renderSubview(
-    new HelpIconView({
-      link: `${config.docs}/${docsLink}`
-    }),
+    new HelpIconView({ link: `${config.docs}/${docsLink}` }),
     modal.queryByHook('title')
   )
 
   modal.on('hidden',() => {
-    wizard.remove()
+    form.remove()
     modal.remove()
   })
 
-  wizard.on('submitted',() => { modal.hide() })
+  form.on('submit', (data) => {
+    App.actions.workflow.create(data)
+    modal.hide()
+  })
+
   modal.show()
-  modal.wizard = wizard
   return modal
 }
-
-const WorkflowCreationWizard = View.extend({
-  template: `<div><section data-hook="form-container"></section></div>`,
-  render () {
-    this.renderWithTemplate(this)
-
-    this.createForm( new Workflow({}) )
-  },
-  /**
-   * @param {Resource} resource resource monitor instance
-   */
-  createForm (workflow) {
-    const form = new WorkflowFormView({ model: workflow })
-    this.renderSubview(form, this.queryByHook('form-container'))
-    this.form = form
-    form.on('submitted', () => { this.trigger('submitted') })
-  },
-  remove () {
-    if (this.form) {
-      this.form.remove()
-    }
-
-    View.prototype.remove.apply(this,arguments)
-  }
-})
