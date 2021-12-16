c0d37796c512ed255ba66108d06f73f5cb34d148
Ezequiel Korenblum <ezequiel@losko.com.ar>
2021-12-16 12:12:15 -0300

    version

--- src/view/page/help/index.js
+++ src/view/page/help/index.js
@@ -1,5 +1,6 @@
 import App from 'ampersand-app'
 import View from 'ampersand-view'
+import XHR from 'lib/xhr'
 import './style.less'
 
 export default View.extend({
@@ -8,11 +9,51 @@ export default View.extend({
       <div class="admin-panel">
         <h3 class="list-title"">Help</h3>
         <div data-hook="help-container" class="help-container">
+          <div class="version">TheEye Web <small data-hook="web-ver">fetching version</small></h3>
+          <div class="version">TheEye Supervisor <small data-hook="sup-ver">fetching version</small></h3>
+          <div class="version">TheEye Gateway <small data-hook="gate-ver">fetching version</small></h3>
           <h4>Need some help?</h4>
           <p>Visit our website: <a class="help-link" target="_blank" href="https://theeye.io/">theeye.io</a></p>
           <p>Our documentation: <a class="help-link" target="_blank" href="https://documentation.theeye.io">documentation.theeye.io</a></p>
         </div>
       </div>
     </div>
-  `
+  `,
+  render() {
+    this.renderWithTemplate(this)
+    
+    XHR.send({
+      url: `${App.config.api_url}/status`,
+      method: 'GET',
+      headers: {
+        Accept: 'application/json;charset=UTF-8'
+      },
+      done: (response,xhr) => {
+        console.log(response)
+        this.queryByHook("gate-ver").innerHTML = response.theeye_version
+      },
+      fail: (err,xhr) => {
+        this.queryByHook("gate-ver").innerHTML = 'unknown version'
+        App.state.alerts.danger('sorry', 'failed to do that')
+      }
+    })
+
+    XHR.send({
+      url: `${App.config.supervisor_api_url}/api/status`,
+      method: 'GET',
+      headers: {
+        Accept: 'application/json;charset=UTF-8'
+      },
+      done: (response,xhr) => {
+        console.log(response)
+        this.queryByHook("sup-ver").innerHTML = response.theeye_version
+      },
+      fail: (err,xhr) => {
+        this.queryByHook("sup-ver").innerHTML = 'unknown version'
+        App.state.alerts.danger('sorry', 'failed to do that')
+      }
+    })
+
+    this.queryByHook("web-ver").innerHTML = 'unknown version'
+  }
 })
