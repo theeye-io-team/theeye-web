32a231671812db8fc0f6eb3bb8fa4901b5c10c6d
Facundo <facugon@theeye.io>
2021-12-22 15:22:24 -0300

    hide and show tabs when has content (#15)

--- src/state/tabs.js
+++ src/state/tabs.js
@@ -2,6 +2,7 @@ import App from 'ampersand-app'
 import State from 'ampersand-state'
 import { Model as Tab, Collection as Tabs } from 'models/tab'
 import * as TabsConstants from 'constants/tabs'
+import loggerModule from 'lib/logger'; const logger = loggerModule('state:tabs')
 
 export default State.extend({
   props: {
@@ -11,51 +12,70 @@ export default State.extend({
     tabs: Tabs
   },
   initialize () {
-    let tabs = {
-      workflows: new Tab({name: TabsConstants.WORKFLOWS}),
-      indicators: new Tab({name: TabsConstants.INDICATORS}),
-      monitors: new Tab({name: TabsConstants.MONITORS}),
-      notifications: new Tab({name: TabsConstants.NOTIFICATIONS})
-    }
-
-    for (let tab in tabs) { this.tabs.add(tabs[tab]) }
-
-    this.listenToAndRun(App.state.dashboard, 'change:indicatorsDataSynced', () => {
-      if (App.state.dashboard.indicatorsDataSynced) {
-        tabs.indicators.show = App.state.indicators.length > 0
-        this.stopListening(App.state.dashboard, 'change:indicatorsDataSynced')
-      }
-    })
-
-    this.listenToAndRun(App.state.indicators, 'add', () => {
-      tabs.indicators.show = true
-    })
-
-    // this.listenToAndRun(App.state.dashboard, 'change:resourcesDataSynced', () => {
-    //   if (App.state.dashboard.resourcesDataSynced) {
-    //     tabs.monitors.show = App.state.resources.length > 0
-    //     this.stopListening(App.state.dashboard, 'change:resourcesDataSynced')
-    //   }
-    // })
-    //
-    // this.listenToAndRun(App.state.dashboard, 'change:tasksDataSynced', () => {
-    //   if (App.state.dashboard.tasksDataSynced) {
-    //     tabs.workflows.show = App.state.tasks.length > 0
-    //     this.stopListening(App.state.dashboard, 'change:tasksDataSynced')
-    //   }
-    // })
+    this.addTab(TabsConstants.WORKFLOWS)
+    this.addTab(TabsConstants.INDICATORS)
+    this.addTab(TabsConstants.MONITORS)
+    this.addTab(TabsConstants.NOTIFICATIONS)
 
     this.listenToAndRun(App.state.dashboard, 'change:dataSynced', () => {
       if (App.state.dashboard.dataSynced) {
-        this.setActiveTab()
+        this.resetActiveTab()
         this.stopListening(App.state.dashboard, 'change:dataSynced')
       }
     })
-  },
-  setActiveTab () {
-    var firstTab = App.state.tabs.tabs.find(function (tab) {
-      return tab.show
+
+    this.listenTo(this.tabs, 'change:show', (tab) => {
+      logger.log(`${tab.name} show changed to ${tab.show}`)
+      if (tab.active === true && tab.show === false) {
+        this.resetActiveTab()
+      }
     })
-    App.actions.tabs.setCurrentTab(firstTab.name)
-  }
+  },
+  resetActiveTab () {
+    const aVisibleTab = this.tabs.find(tab => tab.show === true)
+    if (!aVisibleTab) {
+      logger.log(`Cannot activate any tab`)
+      return
+    }
+    App.actions.tabs.setCurrentTab(aVisibleTab.name)
+    logger.log(`Active tab ${aVisibleTab.name}`)
+  },
+  addTab (name) {
+    const tab = new Tab({ name })
+    this.tabs.add(tab)
+
+    let collectionName
+    let propName
+    if (name === TabsConstants.MONITORS) {
+      collectionName = 'resources'
+      propName = `${collectionName}DataSynced`
+    } else if (name === TabsConstants.WORKFLOWS) {
+      collectionName = TabsConstants.WORKFLOWS
+      propName = `tasksDataSynced`
+    } else {
+      collectionName = name
+      propName = `${name}DataSynced`
+    }
+
+    const collection = App.state[collectionName]
+
+    if (name === TabsConstants.NOTIFICATIONS) {
+      tab.show = false
+    } else {
+      this.listenToAndRun(App.state.dashboard, `change:${propName}`, () => {
+        if (App.state.dashboard[propName]) {
+          tab.show = (collection.length > 0)
+          this.stopListening(App.state.dashboard, `change:${propName}`)
+        }
+      })
+    }
+
+    this.listenToAndRun(collection, 'add', () => {
+      if (collection.length > 0 && tab.show === false) {
+        tab.show = true
+      } else if (collection.length === 0 && tab.show === true) {
+        tab.show = false
+      }
+    })
+  },
 })
