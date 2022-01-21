4c0be1492def334ca5bc1e14ea9f6d9d3c66a08b
Facundo <facugon@theeye.io>
2022-01-21 12:01:14 -0300

    bug: workflow fixes 2 (#24)
    * bug: incluse script on task creation from workflow
    
    * feat: Labels for connecting arrows in workflows
    
    * fix: start_task_id now copying over
    
    * fix: Clicking on nodes works as intended
    
    * fix: You can now remove connections between tasks in workflows
    
    * fix: Tapping in the graph background will remove the menu
    
    * Revert "fix: start_task_id now copying over"
    
    This reverts commit f2751ad6db1070c9994105c71adef9fcc108c735.
    
    * remove unused code
    
    * bug: task creation restored
    
    * fix: is_script property removed from files models
    
    confusing and useless property
    
    * label changed
    
    * Added error reporting
    
    Co-authored-by: Ezequiel Korenblum <ezequiel@losko.com.ar>

--- src/view/workflow/index.js
+++ src/view/workflow/index.js
@@ -96,10 +96,19 @@ export default View.extend({
       })
     })
 
+    let labels = Object.assign({}, graph._edgeLabels)
+
     graph.edges().forEach(function (e) {
       elems.push({
         group: 'edges',
-        data: { source: e.v, target: e.w }
+        data: { source: e.v, target: e.w, label: ((e) => {
+          const key = Object.keys(labels).filter(
+            key => key.includes(e.v) && key.includes(e.w)
+          )[0]
+          const label = labels[key]
+          delete labels[key]
+          return label
+        })(e) }
       })
     })
 
@@ -154,16 +163,34 @@ export default View.extend({
           'target-arrow-shape': 'triangle',
           'line-color': '#9dbaea',
           'target-arrow-color': '#9dbaea',
-          'curve-style': 'bezier'
+          'curve-style': 'bezier',
+          'content': function (ele) {
+            return ele.data('label') || ''
+          },
+          'text-rotation': 'autorotate',
+          'text-margin-y': -10,
+          'color': '#FFF',
+          'font-size': 15,
+          'text-outline-width': 1,
+          'text-outline-color': '#111',
+          'text-opacity': 0.8,
         }
       }]
     })
 
     this.cy = cy
 
-    cy.on('tap', 'node', (event) => {
-      var node = event.cyTarget.data()
-      this.trigger('tap:node', event)
+    cy.on('tap', (event) => {
+      const isNode = typeof event.cyTarget.isNode === 'function' && event.cyTarget.isNode() 
+      const isEdge = typeof event.cyTarget.isEdge === 'function' && event.cyTarget.isEdge() 
+      
+      if (isNode) {
+        this.trigger('tap:node', event)
+      } else if (isEdge) {
+        this.trigger('tap:edge', event)
+      } else {
+        this.trigger('tap:back', event)
+      }
     })
   }
 })
