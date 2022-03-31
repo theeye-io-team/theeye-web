a54fca6441e01fdd3b8bebe07c06f6548730e224
facugon <facugon@theeye.io>
2022-03-31 17:23:02 -0300

    script-import moved to form/script/file-import
    is only being used by the task form

---
+++ src/view/page/task/form/script/file-import.js
@@ -0,0 +1,87 @@
+
+import App from 'ampersand-app'
+import { Model as FileModel } from 'models/file'
+import FileForm from 'view/page/files/form'
+import Modalizer from 'components/modalizer'
+import TaskFormActions from 'actions/taskform'
+import InputView from 'components/input-view/index'
+import DisabledInputView from 'components/input-view/disabled'
+
+export default DisabledInputView.extend({
+  template: `
+    <div>
+      <label class="col-sm-3 control-label" data-hook="label"></label>
+      <div data-hook="input-container" class="col-sm-6">
+        <input class="form-control form-input" style="visibility:hidden;display:none;">
+        <label class="control-label" data-hook="value-container"></label>
+        <div data-hook="message-container" class="message message-below message-error">
+          <p data-hook="message-text"></p>
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <button data-hook="edit-button" class="btn btn-block btn-primary">Edit script</button>
+      </div>
+    </div>
+  `,
+  props: {
+    file: 'object'
+  },
+  initialize (options) {
+    this.disabled = true
+    this.displayValue = options.file.filename
+    options.value = this.displayValue
+    InputView.prototype.initialize.apply(this, arguments)
+    this.listenTo(this, 'change:file', () => {
+      this.displayValue = this.file.filename
+      this.inputValue = this.file.filename
+    })
+  },
+  events: {
+    'click button[data-hook=edit-button]': 'onClickEditButton'
+  },
+  onClickEditButton (event) {
+    if (event) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+
+    let fileModel
+    if (!this.file) {
+      fileModel = new FileModel({ _type: 'Script' })
+    } else {
+      fileModel = new FileModel(this.file, { parse: true })
+      //file = new FileModel( App.state.taskForm.file )
+    }
+
+    const form = new ImportFileForm({ model: fileModel })
+    const modal = new Modalizer({
+      buttons: false,
+      title: 'Script Form',
+      bodyView: form
+    })
+
+    this.listenTo(modal, 'shown', () => { form.focus() })
+    this.listenTo(modal, 'hidden', () => {
+      form.remove()
+      modal.remove()
+    })
+
+    this.listenTo(form, 'submitted', data => {
+      this.file = data
+      this.trigger('submitted', data)
+      modal.hide()
+    })
+
+    modal.show()
+    return false
+  }
+})
+
+const ImportFileForm = FileForm.extend({
+  submitCallback () {
+    let data = this.prepareData(this.data)
+    TaskFormActions.setFile(data)
+    this.trigger('submitted', data)
+  }
+})
+
