05eb9876eaac39eaadbb25a32529d1acd3edfd95
facugon <facugon@theeye.io>
2022-04-28 11:41:40 -0300

    facugon help

--- src/models/policy.js
+++ src/models/policy.js
@@ -12,13 +12,25 @@ const RuleModel = AppModel.extend({
     id: 'string',
     method: 'string',
     path: 'string',
-    params: 'object'
+    //params: 'object',
   }
 })
 
+//
+// default policies
+//
+// FetchTask => GET /task
+// ejemplo:
+//
+// { method: 'get', path: '/task', name: 'FetchTasks' }
+//
+// GetTask => GET /task/${id}
+//
+// { method: 'get', path: '/task/${task}', name: 'GetTask' }
+//
 const RulesCollection = AppCollection.extend({
   indexes: ['id'],
-  Model: RuleModel
+  model: RuleModel
 })
 
 const Model = AppModel.extend({
@@ -35,7 +47,7 @@ const Model = AppModel.extend({
 const Collection = AppCollection.extend({
   url: urlRoot,
   indexes: ['id'],
-  Model: Model
+  model: Model
 })
 
 export { Model, Collection }
--- src/view/iam-menu/groups/form.js
+++ src/view/iam-menu/groups/form.js
@@ -2,9 +2,12 @@ import App from 'ampersand-app'
 import FormView from 'ampersand-form-view'
 import InputView from 'components/input-view'
 import MembersSelectView from 'view/members-select'
+import SelectView from 'components/select2-view'
 
 export default FormView.extend({
   initialize () {
+    const isNewTask = Boolean(this.model.isNew()) // or is import
+
     this.fields = [
       new InputView({
         name: 'name',
@@ -18,13 +21,40 @@ export default FormView.extend({
         name: 'members',
         value: this.model.members,
         idAttribute: 'id',
+      }),
+      new PolicySelectView({
+        value: this.model.policies
       })
     ]
     FormView.prototype.initialize.apply(this, arguments)
   },
-  prepareData() {
-    let data = Object.assign({}, this.data)
-    data.customer = App.state.session.customer.id
-    return data
+  //prepareData() {
+  //  let data = Object.assign({}, this.data)
+  //  data.customer = App.state.session.customer.id
+  //  return data
+  //},
+  submit () {
+    this.beforeSubmit()
+    if (this.valid) {
+      let data = this.data
+      this.trigger('submit', data)
+    }
+  },
+})
+
+const PolicySelectView = SelectView.extend({
+  initialize (specs) {
+    this.options = App.state.policies 
+    this.multiple = true
+    this.tags = false
+    this.label = 'Policy Selection'
+    this.name = 'policies'
+    this.styles = 'form-group'
+    this.unselectedText = 'select a policy'
+    this.idAttribute = 'id'
+    this.textAttribute = 'name'
+    this.allowCreateTags = false
+
+    SelectView.prototype.initialize.apply(this, arguments)
   }
 })
