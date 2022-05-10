4716a27b1d609658b194c02e780fcf353a2cf8de
Ezequiel Korenblum <ezequiel@losko.com.ar>
2022-05-10 17:02:38 -0300

    Adding a button for the policy selector

--- src/view/iam-menu/policy-selector.js
+++ src/view/iam-menu/policy-selector.js
@@ -1,7 +1,25 @@
 import App from 'ampersand-app'
 import SelectView from 'components/select2-view'
+import Modalizer from 'components/modalizer'
+import PolicyForm from './policies/form'
 
 export default SelectView.extend({
+  template: `
+    <div>
+      <div>
+        <label data-hook="label" class="col-sm-3 control-label"></label>
+        <div class="col-sm-6">
+          <select class="form-control select" style="width:100%"></select>
+          <div data-hook="message-container" class="message message-below message-error">
+            <p data-hook="message-text"></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <button data-hook="create-button" class="btn btn-block btn-primary">Create a policy</button>
+      </div>
+    </div>
+  `,
   initialize (specs) {
     this.options = App.state.policies.models
     this.multiple = true
@@ -13,8 +31,38 @@ export default SelectView.extend({
     this.idAttribute = 'id'
     this.textAttribute = 'name'
     this.allowCreateTags = false
+    this.required = specs?.required || false
 
     SelectView.prototype.initialize.apply(this, arguments)
+  },
+  events: {
+    'click button[data-hook=mode-button]':'onClickModeButton'
+  },
+  onClickModeButton (event) {
+    event.preventDefault()
+    event.stopPropagation()
+
+    const policyForm = new PolicyForm({ model: new App.Models.Policy.Model() })
+    const policyModal = new Modalizer({
+      title: 'Create Policy',
+      bodyView: policyForm,
+      buttons: true,
+      confirmButton: 'Create Policy'
+    })
+    // this.listenTo(policyModal, 'shown', function () { policyForm.focus() })
+    this.listenTo(policyModal, 'hidden', function () {
+      policyForm.remove()
+      policyModal.remove()
+    })
+    this.listenTo(policyModal, 'confirm', function () {
+      policyForm.beforeSubmit()
+      if (!policyForm.valid) return
+
+      let data = policyForm.prepareData()
+      App.state.policies.create(data)
+      policyModal.hide()
+    })
+    policyModal.show()
   },
   derived: {
     value: {
