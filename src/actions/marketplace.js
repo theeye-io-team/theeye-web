3b625fab04dd57bfee3c449ac46395edddd418fd
Ezequiel Korenblum <ezequiel@losko.com.ar>
2022-08-17 16:15:40 -0300

    Marketplace actions

---
+++ src/actions/marketplace.js
@@ -0,0 +1,61 @@
+import App from 'ampersand-app'
+import XHR from 'lib/xhr'
+
+export default {
+  menu: {
+    hide () {
+      App.state.marketplace.menu.visible = false
+    },
+    show () {
+      let state = App.state.marketplace.menu
+      state.current_tab = state.default_tab
+      state.visible = true 
+    },
+    toggle () {
+      App.state.marketplace.menu.toggle('visible')
+    },
+    toggleTab (tabId) {
+      App.state.marketplace.menu.current_tab = tabId
+    }
+  },
+  tasks: {
+    fetch () {
+      XHR.send({
+        method: 'GET',
+        url: `${App.config.marketplace_api.url}/task?unassigned=true`,
+        authorization: `Bearer ${App.config.marketplace_api.bearer}`,
+        done (tasks) {
+          App.state.marketplace.tasks.list = tasks.map(task => { 
+            return {
+              name: task.name,
+              id: task.id,
+              description: task.description,
+              type: task.type
+            }
+          })
+          App.state.marketplace.tasks.fetched = true
+        },
+        fail (err, xhr) {
+          let msg = 'Error retrieving tasks from the Marketplace.'
+          bootbox.alert(msg)
+          return next(new Error(msg))
+        }
+      })
+    },
+    getRecipe (id) {
+      return new Promise((resolve, reject) => {
+        XHR.send({
+          method: 'GET',
+          url: `${App.config.marketplace_api.url}/task/${id}/serialize?mode=shallow`,
+          authorization: `Bearer ${App.config.marketplace_api.bearer}`,
+          done (recipe) {
+            resolve(recipe)
+          },
+          fail (err) {
+            reject(err)
+          }
+        })
+      })
+    }
+  }
+}
