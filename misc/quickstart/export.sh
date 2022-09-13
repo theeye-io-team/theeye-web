eedb988029794a27dbb8bc9669209d75035f6803
facugon <facugon@theeye.io>
2022-09-13 14:00:41 -0300

    feat: quickstart. docker export script and config

---
+++ misc/quickstart/export.sh
@@ -0,0 +1,7 @@
+# export sources
+
+cp -r /app/dist/. /output
+
+sed -i 's|<\!--\[if true\]><script src\="\/config\.js"><\/script><\!\[endif\]-->|<script src="/config.js"></script>|' /output/index.html
+
+cp local.js /output/config.js
