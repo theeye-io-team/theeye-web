9d92e53c0eda12cb0382ed0eaea258217ee54e2c
facugon <facugon@theeye.io>
2022-09-13 17:37:13 -0300

    fix: use absolute path

--- misc/quickstart/export.sh
+++ misc/quickstart/export.sh
@@ -4,4 +4,4 @@ cp -r /app/dist/. /output
 
 sed -i 's|<\!--\[if true\]><script src\="\/config\.js"><\/script><\!\[endif\]-->|<script src="/config.js"></script>|' /output/index.html
 
-cp local.js /output/config.js
+cp /app/misc/quickstart/local.js /output/config.js
