b8af99620ab1037496b041d78be8b9a7b82d4aca
facugon <facugon@theeye.io>
2023-01-03 13:20:35 -0300

    customer configuration is not required anymore

--- src/models/customer.js
+++ src/models/customer.js
@@ -2,32 +2,11 @@ import App from 'ampersand-app'
 import AppModel from 'lib/app-model'
 import AppCollection from 'lib/app-collection'
 import config from 'config'
-import merge from 'lodash/merge'
 
 const urlRoot = function () {
   return `${config.api_url}/admin/customer`
 }
 
-const defaultConfig = {
-  kibana: {
-    enabled: false,
-    url: ''
-  },
-  elasticsearch: {
-    enabled: false,
-    url: ''
-  },
-  ngrok: {
-    enabled: false,
-    address: '',
-    authtoken: '',
-    protocol: ''
-  },
-  enterprise_login: {
-    enabled: false, 
-    url: ''
-  }
-}
 
 const Model = AppModel.extend({
   urlRoot,
@@ -36,9 +15,7 @@ const Model = AppModel.extend({
     name: 'string',
     display_name: 'string',
     description: 'string',
-    config: ['object', true, () => {
-      return Object.assign({}, defaultConfig)
-    }],
+    config: 'object',
     creation_date: 'date',
     last_update: 'date'
   },
@@ -75,22 +52,6 @@ const Model = AppModel.extend({
         return this.name
       }
     }
-  },
-  parse (attrs) {
-    // MUTATE kibana config schema
-    // special `if`: when Factory parses the value, config is empty
-    if (attrs.config) {
-      attrs.config.kibana = attrs.config.kibana || defaultConfig.kibana
-      if (typeof (attrs.config.kibana) === 'string') {
-        attrs.config.kibana = {
-          enabled: true,
-          url: attrs.config.kibana || ''
-        }
-      }
-    }
-
-    attrs.config = merge({}, defaultConfig, attrs.config)
-    return attrs
   }
 })
 
--- src/view/page/customer/form.js
+++ src/view/page/customer/form.js
@@ -1,38 +1,13 @@
 import FormView from 'ampersand-form-view'
 import InputView from 'components/input-view'
-import CheckboxView from 'components/checkbox-view'
-import SelectView from 'components/select2-view'
-import isURL from 'validator/lib/isURL'
-import isEmail from 'validator/lib/isEmail'
 
 import App from 'ampersand-app'
 
 export default FormView.extend({
-  props: {
-    showKibana: ['boolean', false, false],
-    kibanaUrlInput: 'object'
-  },
-  initialize: function (options) {
-    var isNew = this.model.isNew()
+  initialize (options) {
+    const isNew = this.model.isNew()
 
-    this.showKibana = (!isNew ? this.model.config.kibana.enabled : false)
     this.fields = [
-      //new InputView({
-      //  name: 'name',
-      //  label: 'Name',
-      //  value: this.model.name,
-      //  required: true,
-      //  invalidClass: 'text-danger',
-      //  validityClassSelector: '.control-label',
-      //  readonly: !isNew,
-      //  tests: [
-      //    (value) => {
-      //      if (!isEmail(`${value}@theeye.io`)) {
-      //        return 'Please provide a valid customer name'
-      //      }
-      //    }
-      //  ]
-      //}),
       new InputView({
         name: 'display_name',
         label: 'Display Name',
@@ -49,97 +24,13 @@ export default FormView.extend({
         required: false,
         invalidClass: 'text-danger',
         validityClassSelector: '.control-label'
-      }),
-      new CheckboxView({
-        name: 'kibana_enabled',
-        label: 'Kibana enabled',
-        value: (!isNew ? this.model.config.kibana.enabled : false)
-      }),
-      this.kibanaUrlInput = new InputView({
-        name: 'kibana_url',
-        label: 'Kibana iframe',
-        placeholder: 'Kibana iframe',
-        visible: this.showKibana,
-        value: (!isNew ? this.model.config.kibana.url : ''),
-        invalidClass: 'text-danger',
-        validityClassSelector: '.control-label',
-        required: this.showKibana
-      }),
-      new CheckboxView({
-        name: 'elasticsearch_enabled',
-        label: 'Elasticsearch enabled',
-        value: (!isNew ? this.model.config.elasticsearch.enabled : false)
       })
     ]
-
-    if (!isNew && this.model.config.elasticsearch.enabled) {
-      const elasticsearchUrl = new InputView({
-        name: 'elasticsearch_url',
-        label: 'Elasticsearch url',
-        placeholder: 'Elastic search url',
-        invalidClass: 'text-danger',
-        validityClassSelector: '.control-label',
-        required: true,
-        value: this.model.config.elasticsearch.url,
-        tests: [
-          function (value) {
-            if(!isURL(value,{
-              protocols: ['http','https'],
-              require_protocol: true
-            })) {
-              return "Must be a valid URL (include protocol)"
-            }
-          }
-        ]
-      })
-
-      this.fields.push(elasticsearchUrl)
-    }
 
     FormView.prototype.initialize.apply(this, arguments)
   },
   render () {
     FormView.prototype.render.apply(this, arguments)
     this.query('form').classList.add('form-horizontal')
-  },
-  events: {
-    'change input[name=elasticsearch_enabled]': function (event) {
-      this.togglePasswordFields(event.target.checked)
-    },
-    'change input[name=kibana_enabled]': function (event) {
-      this.showKibana = event.target.checked
-      this.kibanaUrlInput.set({visible: this.showKibana, required: this.showKibana})
-    }
-  },
-  togglePasswordFields: function (on) {
-    var isNew = this.model.isNew()
-    var urlValue = (!isNew && this.model.config.elasticsearch.url) ? this.model.config.elasticsearch.url : ''
-
-    if (on) {
-      const elasticsearchUrl = new InputView({
-        name: 'elasticsearch_url',
-        label: 'Elasticsearch url',
-        placeholder: 'Elastic search url',
-        invalidClass: 'text-danger',
-        validityClassSelector: '.control-label',
-        required: true,
-        value: urlValue,
-        tests: [
-          function (value) {
-            if(!isURL(value,{
-              protocols: ['http','https'],
-              require_protocol: true
-            })) {
-              return "Must be a valid URL (include protocol)"
-            }
-          }
-        ]
-      })
-      this.addField(elasticsearchUrl)
-      this.renderField(elasticsearchUrl)
-    } else {
-      this.getField('elasticsearch_url').remove()
-      this.removeField('elasticsearch_url')
-    }
   }
 })
