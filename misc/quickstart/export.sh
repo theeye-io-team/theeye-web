03d62699bf4bea58d679f9e03429b692c3582ae7
facugon <facugon@theeye.io>
2023-10-03 08:14:43 -0300

    ci(webpack): moved static files to static dir
                 added default config file

--- misc/quickstart/export.sh
+++ misc/quickstart/export.sh
@@ -2,6 +2,6 @@
 
 cp -r /app/dist/. /output
 
-sed -i 's|<\!--\[if true\]><script src\="\/config\.js"><\/script><\!\[endif\]-->|<script src="/config.js"></script>|' /output/index.html
+#sed -i 's|<\!--\[if true\]><script src\="\/config\.js"><\/script><\!\[endif\]-->|<script src="/config.js"></script>|' /output/index.html
 
 cp /app/misc/quickstart/local.js /output/config.js
