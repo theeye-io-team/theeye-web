18d03c34cf10761b33f29e56583d58d37bcf7092
facugon <facugon@theeye.io>
2023-11-29 18:51:45 -0300

    fix: deploy static configs

--- static/config.js
+++ static/config.js
@@ -2,10 +2,11 @@
 // add your own env settings
 //
 //
-window.config = {
+window.config = null
+  //{
   //app_url: "http://gateway url",
   //socket_url: "http://gateway url",
   //api_url: "http://gateway url/api",
   //api_v3_url: "http://gateway url/api",
   //supervisor_api_url: "http://supervisor url",
-}
+  //}
