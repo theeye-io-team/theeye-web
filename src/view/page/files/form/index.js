775079f24e7071238ccdd00edeeec860fda90019
facugon <facugon@theeye.io>
2023-12-21 17:06:24 -0300

    function

--- src/view/page/files/form/index.js
+++ src/view/page/files/form/index.js
@@ -2,7 +2,7 @@ import AllLanguages from './all_lang'
 import Nodejs from './node'
 import * as TaskConstants from 'constants/task'
 
-export default (specs) => {
+export default function (specs) {
   if (specs.language === TaskConstants.TYPE_NODEJS) {
     return new Nodejs(specs)
   } else {
